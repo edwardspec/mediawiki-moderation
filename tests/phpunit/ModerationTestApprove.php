44356635f195800c317f9da7266ca5c48ff51d65
Edward Chernenko <edwardspec@gmail.com>
2015-02-22 21:38:24 +0300

    (tests) Implement ApproveAll test

--- tests/phpunit/ModerationTestApprove.php
+++ tests/phpunit/ModerationTestApprove.php
@@ -72,4 +72,53 @@ class ModerationTestApprove extends MediaWikiTestCase
 		$this->assertEquals($t->lastEdit['User'], $t->deleted_entries[0]->user);
 		$this->assertEquals($t->lastEdit['Title'], $t->deleted_entries[0]->title);
 	}
+
+	public function testApproveAll() {
+		$t = new ModerationTestsuite();
+		$t->fetchSpecial();
+
+		# We edit with two users:
+		#	$t->unprivilegedUser (A)
+		#	and $t->unprivilegedUser2 (B)
+		# We're applying approveall to one of the edits by A.
+		# Expected result is:
+		# 1) All edits by A were approved,
+		# 2) No edits by B were touched during approveall.
+
+		$t->doNTestEditsWith($t->unprivilegedUser, $t->unprivilegedUser2);
+		$t->fetchSpecialAndDiff();
+
+		# Find edits by user A (they will be approved)
+		$entries = ModerationTestsuiteEntry::findByUser(
+			$t->new_entries,
+			$t->unprivilegedUser
+		);
+
+		$req = $t->makeHttpRequest($entries[0]->approveAllLink, 'GET');
+		$this->assertTrue($req->execute()->isOK());
+
+		/* TODO: check $req->getContent() */
+
+		$t->fetchSpecialAndDiff();
+		$this->assertCount(0, $t->new_entries,
+			"testApproveAll(): Something was added into Pending folder during modaction=approveall");
+		$this->assertCount($t->TEST_EDITS_COUNT, $t->deleted_entries,
+			"testApproveAll(): Several edits were approved, but number of deleted entries in Pending folder doesn't match");
+
+		foreach($entries as $entry)
+		{
+			$ret = $t->query(array(
+				'action' => 'query',
+				'prop' => 'revisions',
+				'rvlimit' => 1,
+				'rvprop' => 'user|timestamp|comment|content',
+				'titles' => $entry->title
+			));
+			$ret_page = array_shift($ret['query']['pages']);
+			$rev = $ret_page['revisions'][0];
+
+			$this->assertEquals($t->unprivilegedUser->getName(), $rev['user']);
+		}
+
+	}
 }
