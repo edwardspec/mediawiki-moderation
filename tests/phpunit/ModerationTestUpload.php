7a15272c355a7c9079c464ef2dceb971738cbd00
Edward Chernenko <edwardspec@gmail.com>
2015-03-15 01:34:36 +0300

    (tests) Implement Reupload test

--- tests/phpunit/ModerationTestUpload.php
+++ tests/phpunit/ModerationTestUpload.php
@@ -88,4 +88,90 @@ class ModerationTestUpload extends MediaWikiTestCase
 
 		$this->assertEquals('(emptyfile)', $error);
 	}
+
+	public function testReupload() {
+		$t = new ModerationTestsuite();
+		$title = "Test image 1.png";
+
+		# Upload the image first
+		$t->loginAs($t->automoderated);
+		$t->doTestUpload($title, __DIR__ . "/../resources/image640x50.png");
+
+		# Now queue reupload for moderation
+		$t->loginAs($t->unprivilegedUser);
+		$error = $t->doTestUpload($title, __DIR__ . "/../resources/image100x100.png");
+		$t->fetchSpecial();
+
+		# Was the reupload queued for moderation?
+		$this->assertEquals('(moderation-image-queued)', $error);
+
+		# Is the data on Special:Moderation correct?
+		$entry = $t->new_entries[0];
+		$this->assertCount(1, $t->new_entries,
+			"testReupload(): One upload was queued for moderation, but number of added entries in Pending folder isn't 1");
+		$this->assertCount(0, $t->deleted_entries,
+			"testReupload(): Something was deleted from Pending folder during the queueing");
+		$this->assertEquals($t->lastEdit['User'], $entry->user);
+		$this->assertEquals($t->lastEdit['Title'], $entry->title);
+
+		# Can we approve this reupload?
+		$this->assertNotNull($entry->approveLink,
+			"testReupload(): Approve link not found");
+
+		$req = $t->makeHttpRequest($entry->approveLink, 'GET');
+		$this->assertTrue($req->execute()->isOK());
+
+		/* TODO: check $req->getContent() */
+
+		# Has the file been reuploaded after the approval?
+		$ret = $t->query(array(
+			'action' => 'query',
+			'prop' => 'imageinfo',
+			'iilimit' => 1,
+			'iiprop' => 'user|timestamp|comment|size|url|sha1',
+			'titles' => $entry->title
+		));
+		$ret_page = array_shift($ret['query']['pages']);
+		$ii = $ret_page['imageinfo'][0];
+
+		$this->assertEquals($t->lastEdit['User'], $ii['user']);
+		$this->assertEquals($t->lastEdit['Text'], $ii['comment']);
+		$this->assertEquals($t->lastEdit['SHA1'], $ii['sha1']);
+
+		# Check image page history: performUpload(... $user) mistakenly
+		# tags image reuploads as made by moderator (and not $user).
+		# Was that fixed?
+
+		$ret = $t->query(array(
+			'action' => 'query',
+			'prop' => 'revisions',
+			'rvlimit' => 2, # See below
+			'rvprop' => 'user|timestamp|comment|content|ids',
+			'titles' => $entry->title
+		));
+
+		# Because API orders entries by timestamp (up to seconds), and
+		# it's likely that two uploads we just made will have the same
+		# timestamp, they may be ordered incorrectly ([0] not being the
+		# most recent). So find the entry with 'parentid' referring to
+		# the other entry.
+		$ret_page = array_shift($ret['query']['pages']);
+		$rev1 = $ret_page['revisions'][0];
+		$rev2 = $ret_page['revisions'][1];
+
+		# Make $rev1 the most recent edit
+		if($rev2['parentid'] == $rev1['revid'])
+		{
+			$tmp = $rev1;
+			$rev1 = $rev2;
+			$rev2 = $tmp;
+		}
+
+		$this->assertEquals($rev2['revid'], $rev1['parentid'],
+			"testReupload(): parentid of new revision doesn't match revid of the previous revision");
+		$this->assertNotEquals($t->moderator->getName(), $rev1['user'],
+			"testReupload(): Image reupload was attributed to the moderator who approved it (instead of the user who made the reupload)");
+		$this->assertEquals($t->lastEdit['User'], $rev1['user'],
+			"testReupload(): Image reupload wasn't attributed to the user who made it");
+	}
 }
