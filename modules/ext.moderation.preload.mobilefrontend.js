5d8c2b19d8ded34694d14b2c66d1122fb14b818b
Edward Chernenko <edwardspec@gmail.com>
2016-12-12 19:27:31 +0300

    (mobilefrontend) Implement preload for MobileFrontend

--- modules/ext.moderation.preload.mobilefrontend.js
+++ modules/ext.moderation.preload.mobilefrontend.js
@@ -7,16 +7,64 @@
 	has a pending change, it won't be preloaded at all.
 */
 
-( function ( mw, $ ) {
+( function ( M, $ ) {
 	'use strict';
 
-	/* Supply MobileFrontend with preloaded pending edit, if there is one */
-	mw.loader.using( 'mobile.editor.common', function() {
+	var EditorGateway = M.require( 'mobile.editor.api/EditorGateway' ),
+		oldGetContent = EditorGateway.prototype.getContent;
 
-		var api = new mw.Api();
+	EditorGateway.prototype.getContent = function() {
 
-		/* TODO */
-	});
+		/*
+			useDefault() - call the original (unmodified) method from EditorGateway.
+			Example: return useDefault( "no change is awaiting moderation, so nothing to preload!" );
+		*/
+		var useDefault = function( reason ) {
+			console.log( "Moderation: not preloading: " + reason );
+			return oldGetContent.apply( this, arguments );
+		};
 
-}( mediaWiki, jQuery ) );
+		/* Only load once */
+		if ( this.content !== undefined ) {
+			return useDefault( "already loaded" );
+		}
 
+		/* If user is editing some older revision,
+			then preloading is not needed here */
+		if ( this.oldId ) {
+			return useDefault( "user is editing an older revision" );
+		}
+
+		/* Get the wikitext of pending change, if it exists */
+		var result = $.Deferred(),
+			self = this;
+		var qPreload = {
+			action: 'query',
+			prop: 'moderationpreload',
+			mptitle: this.title,
+			mpmode: 'wikitext',
+
+			// MobileFrontend also needs block information for this user
+			meta: 'userinfo',
+			uiprop: 'blockinfo'
+		};
+		this.api.get( qPreload ).then( function( data ) {
+			var wikitext = data.query.moderationpreload.wikitext;
+			if ( !wikitext ) {
+				/* Nothing to preload.
+					Call the original getContent() from EditorGateway. */
+				$result.resolve( useDefault( "no pending change found" ) );
+				return;
+			}
+
+			self.content = wikitext;
+			self.timestamp = ""; /* Ok to leave empty */
+			self.originalContent = self.content;
+
+			result.resolve( self.content, data.query.userinfo );
+		} );
+
+		return result;
+	};
+
+}( mw.mobileFrontend, jQuery ) );
