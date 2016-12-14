0254137b842ec6acc75b3fade7943b59af009124
Edward Chernenko <edwardspec@gmail.com>
2016-12-14 03:11:20 +0300

    (mobilefrontend) Fix legacy mobile notification (MediaWiki 1.23)

--- modules/ext.moderation.notify.js
+++ modules/ext.moderation.notify.js
@@ -44,11 +44,17 @@
 				'.toast, .mw-notification-tag-toast { display: none ! important; }'
 			);
 
+			/* Workaround the bug which affected mw.notify() in MediaWiki 1.23 */
+			var $oldContent = mw.util.$content;
+			mw.util.$content = $( 'body' );
+
 			/* Mobile version */
 			mw.notify( $div, {
 				tag: 'modqueued',
 				autoHide: false,
 				type: 'info'
+			} ).done( function() {
+				mw.util.$content = $oldContent; /* Restore mw.util.$content */
 			} );
 
 			/* If MobileFrontend hasn't reloaded the page after edit,
@@ -56,8 +62,10 @@
 				so that it won't be shown after reload.
 			*/
 			try {
-				var toast = M.require( 'mobile.toast/toast' );
-				toast._showPending();
+				mw.loader.using( 'mobile.toast', function() {
+					var toast = M.require( 'mobile.toast/toast' );
+					toast._showPending();
+				} );
 			} catch ( e ) {
 				 /* Nothing to do - old MobileFrontend (e.g. for MediaWiki 1.23)
 					didn't have "show after reload" anyway.
