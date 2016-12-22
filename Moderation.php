ddcd3193c57f4909d11cef9a7ff5df9a0a96edcd
Edward Chernenko <edwardspec@gmail.com>
2016-12-22 22:51:07 +0300

    (version) 22dec2016-1

--- Moderation.php
+++ Moderation.php
@@ -35,7 +35,7 @@ $wgExtensionCredits['antispam'][] = array(
 	'author' => 'Edward Chernenko',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Moderation',
 	'descriptionmsg' => 'moderation-desc',
-	'version' => '16dec2016-2'
+	'version' => '22dec2016-1'
 );
 
 $wgMessagesDirs['Moderation'] = array(
