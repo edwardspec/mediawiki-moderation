64b238e6d6ba880021fe82ce30c5e4f81e2c0042
Edward Chernenko <edwardspec@gmail.com>
2017-03-19 15:19:46 +0300

    (tests) CheckUser test: ensure that UserAgent of uploads is preserved during modaction=approveall

--- tests/phpunit/ModerationCheckuserTest.php
+++ tests/phpunit/ModerationCheckuserTest.php
@@ -2,7 +2,7 @@
 
 /*
 	Extension:Moderation - MediaWiki extension.
-	Copyright (C) 2015 Edward Chernenko.
+	Copyright (C) 2015-2017 Edward Chernenko.
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -22,8 +22,14 @@
 
 require_once( __DIR__ . "/../ModerationTestsuite.php" );
 
+/**
+	@covers ModerationApproveHook
+*/
 class ModerationTestCheckuser extends MediaWikiTestCase
 {
+	public $moderatorUA = 'UserAgent of Moderator/1.0';
+	public $userUA = 'UserAgent of UnprivilegedUser/1.0';
+
 	public function testModerationCheckuser() {
 		$t = new ModerationTestsuite();
 		$entry = $t->getSampleEntry();
@@ -43,12 +49,8 @@ class ModerationTestCheckuser extends MediaWikiTestCase
 			"testModerationCheckuser(): incorrect IP on Special:Moderation" );
 	}
 
-	/**
-		@covers ModerationCheckUserHook
-	*/
-	public function testPreverveUserAgent() {
+	public function skipIfNoCheckuser() {
 		global $wgSpecialPages;
-		$t = new ModerationTestsuite();
 
 		$dbw = wfGetDB( DB_MASTER );
 		if ( !array_key_exists( 'CheckUser', $wgSpecialPages )
@@ -56,30 +58,78 @@ class ModerationTestCheckuser extends MediaWikiTestCase
 		{
 			$this->markTestSkipped( 'Test skipped: CheckUser extension must be installed to run it.' );
 		}
+	}
 
-		$moderatorUA = 'UserAgent of Moderator/1.0';
-		$userUA = 'UserAgent of UnprivilegedUser/1.0';
+	/**
+		@brief Convenience function: get cuc_agent of the last entry in "cu_changes" table.
+		@param $where Extra conditions for SQL query.
+	*/
+	public function getCUCAgent( $where = array( '1' ) ) {
+		$dbw = wfGetDB( DB_MASTER );
+		return $dbw->selectField( 'cu_changes', 'cuc_agent',
+			$where,
+			__METHOD__,
+			array(
+				'ORDER BY' => 'cuc_id DESC',
+				'LIMIT' => 1
+			)
+		);
+	}
+
+	/**
+		@brief Ensure that modaction=approve preserves user-agent of edits.
+	*/
+	public function testApproveEditPrevervesUA() {
+		$this->skipIfNoCheckuser();
+		$t = new ModerationTestsuite();
 
 		# When the edit is approved, cu_changes.cuc_agent field should
 		# contain UserAgent of user who made the edit,
 		# not UserAgent or the moderator who approved it.
 
-		$t->setUserAgent( $userUA );
+		$t->setUserAgent( $this->userUA );
 		$entry = $t->getSampleEntry();
 
-		$t->setUserAgent( $moderatorUA );
+		$t->setUserAgent( $this->moderatorUA );
 		$t->httpGet( $entry->approveLink );
 
-		$row = $dbw->selectRow( 'cu_changes',
-			array( 'cuc_agent AS agent' ),
-			array( '1' ),
-			__METHOD__,
-			array( 'ORDER BY' => 'cuc_id DESC', 'LIMIT' => 1 )
-		);
+		$agent = $this->getCUCAgent();
+		$this->assertNotEquals( $this->moderatorUA, $agent,
+			"testApproveEditPrevervesUA(): UserAgent in checkuser tables matches moderator's UserAgent" );
+		$this->assertEquals( $this->userUA, $agent,
+			"testApproveEditPrevervesUA(): UserAgent in checkuser tables doesn't match UserAgent of user who made the edit" );
+	}
 
-		$this->assertNotEquals( $moderatorUA, $row->agent,
-			"testPreverveUserAgent(): UserAgent in checkuser tables matches moderator's UserAgent" );
-		$this->assertEquals( $userUA, $row->agent,
-			"testPreverveUserAgent(): UserAgent in checkuser tables doesn't match UserAgent of user who made the edit" );
+	/**
+		@brief Ensure that modaction=approveall preserves user-agent of uploads.
+	*/
+	public function testApproveAllUploadPrevervesUA() {
+		$this->skipIfNoCheckuser();
+		$t = new ModerationTestsuite();
+
+		# 1. Perform upload.
+		$t->loginAs( $t->unprivilegedUser );
+		$t->setUserAgent( $this->userUA );
+		$t->doTestUpload();
+		$t->fetchSpecial();
+		$entry = $t->new_entries[0];
+
+		# 2. Perform another edit: we need to make sure that ModerationApproveHook:getTask()
+		# will work correctly during ApproveAll (even if this upload wasn't the last of the changes).
+		$t->doTestEdit();
+
+		# When the upload is approved, cu_changes.cuc_agent field should
+		# contain UserAgent of user who made the edit,
+		# not UserAgent or the moderator who approved it.
+		$t->setUserAgent( $this->moderatorUA );
+		$t->httpGet( $entry->approveAllLink ); # Try modaction=approveall
+
+		$agent = $this->getCUCAgent( array(
+			'cuc_type' => RC_LOG /* We need to check the upload, so we ignore the non-upload edit */
+		) );
+		$this->assertNotEquals( $this->moderatorUA, $agent,
+			"testApproveAllUploadPrevervesUA(): UserAgent in checkuser tables matches moderator's UserAgent" );
+		$this->assertEquals( $this->userUA, $agent,
+			"testApproveAllUploadPrevervesUA(): UserAgent in checkuser tables doesn't match UserAgent of user who made the edit" );
 	}
 }
