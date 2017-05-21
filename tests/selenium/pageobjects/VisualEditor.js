eec13159c4581ab9daefec9392734094b24a5008
Edward Chernenko <edwardspec@gmail.com>
2017-05-21 21:50:01 +0300

    (tests) Selenium testsuite: improve "waiting for elements" code

--- tests/selenium/pageobjects/VisualEditor.js
+++ tests/selenium/pageobjects/VisualEditor.js
@@ -8,19 +8,11 @@ const Page = require( './page' );
 class VisualEditor extends Page {
 
 	/** @brief Editable element in the editor */
-	get content() { return $( '.ve-ce-documentNode' ); }
+	get content() { return this.getWhenVisible( '.ve-ce-documentNode' ); }
 
-	/** @brief "Save page" button in the editor. */
-	get saveButtonEvenDisabled() { return $( 'a=Save page' ); }
-
-	/** @brief "Describe what you changed" dialog */
-	get confirmDialog() { return $( '.oo-ui-processDialog-navigation' ); }
-
-	/**
-		@brief Returns "Save page" button when it becomes enabled.
-	*/
+	/** @brief "Save page" button in the editor */
 	get saveButton() {
-		var $submit = this.saveButtonEvenDisabled;
+		var $submit = this.getWhenVisible( 'a=Save page' );
 
 		browser.waitUntil( function() {
 			return ( $submit.getAttribute( 'aria-disabled' ) === 'false' );
@@ -31,19 +23,11 @@ class VisualEditor extends Page {
 
 	/** @brief "Save page" button in "Describe what you changed" dialog */
 	get confirmButton() {
-		this.confirmDialog.waitForExist();
-
-		var $submit = this.confirmDialog.element( 'a=Save page' );
-		$submit.waitForVisible();
-		return $submit;
+		return this.getWhenVisible( '//*[@class="oo-ui-processDialog-navigation"]//a[node() = "Save page"]' );
 	}
 
 	open( name ) {
 		super.open( name + '?veaction=edit&vehidebetadialog=true' );
-
-		/* Wait for VisualEditor to be completely rendered */
-		this.content.waitForExist();
-		this.saveButtonEvenDisabled.waitForExist();
 	}
 
 	edit( name, content ) {
--- tests/selenium/pageobjects/page.js
+++ tests/selenium/pageobjects/page.js
@@ -1,11 +1,21 @@
-// From http://webdriver.io/guide/testrunner/pageobjects.html
+/**
+	@brief Most basic class for "Page Object" pattern.
+	@see http://webdriver.io/guide/testrunner/pageobjects.html
+*/
+
 'use strict';
+
 class Page {
 	constructor() {
 		this.title = 'My Page';
 	}
 	open( path ) {
 		browser.url( '/wiki/' + path );
+	}
+
+	getWhenVisible( selector ) {
+		browser.waitForVisible( selector );
+		return $( selector );
 	}
 }
 module.exports = Page;
