b51b41b133c92fc666c269acfd32ff49cb493bb2
Edward Chernenko <edwardspec@gmail.com>
2017-05-21 18:17:17 +0300

    (cosmetic) Minor style fix: adjust CSS margin

--- Moderation.php
+++ Moderation.php
@@ -36,7 +36,7 @@ $wgExtensionCredits['antispam'][] = array(
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Moderation',
 	'descriptionmsg' => 'moderation-desc',
 	'license-name' => 'GPL-3.0+',
-	'version' => '1.0.26'
+	'version' => '1.0.27'
 );
 
 $wgMessagesDirs['Moderation'] = array(
