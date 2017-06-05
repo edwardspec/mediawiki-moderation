2a9ead3944388a245ce8b5deeefda0fdf2afb779
Edward Chernenko <edwardspec@gmail.com>
2017-06-06 00:13:18 +0300

    (visualeditor) Load preload.ve.js early, so that VisualEditor won't call unmodified requestParsoidData() on slow hardware
    A quick fix for a possible race condition when VisualEditor calls requestParsoidData()
    before our script (preload.ve.js) has a chance to wrap around this function.
    
    This fix is primarily for testing in Sauce Labs (see issue #13).
    When not running in a tiny virtual machine, this race condition wasn't observed.

--- Moderation.php
+++ Moderation.php
@@ -36,7 +36,7 @@ $wgExtensionCredits['antispam'][] = array(
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Moderation',
 	'descriptionmsg' => 'moderation-desc',
 	'license-name' => 'GPL-3.0+',
-	'version' => '1.0.30'
+	'version' => '1.0.31'
 );
 
 $wgMessagesDirs['Moderation'] = array(
@@ -111,6 +111,7 @@ $wgResourceModules['ext.moderation.ve'] = $moduleTemplate + array(
 		'ext.moderation.ajaxhook'
 	)
 );
+$wgResourceModules['ext.moderation.ve']['position'] = 'top';
 $wgResourceModules['ext.moderation.mf'] = $moduleTemplate + array(
 	'scripts' => array(
 		'mobilefrontend/legacy.ajaxhook.mf.js',
