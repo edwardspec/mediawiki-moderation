19c8eed218a2a86cafc1c52c7eb155cfe1c53b96
Edward Chernenko <edwardspec@gmail.com>
2017-07-09 01:46:07 +0300

    (i18n) Add link to Special:Log/moderation on Special:Moderation

--- Moderation.php
+++ Moderation.php
@@ -36,7 +36,7 @@ $wgExtensionCredits['antispam'][] = array(
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Moderation',
 	'descriptionmsg' => 'moderation-desc',
 	'license-name' => 'GPL-3.0+',
-	'version' => '1.0.36'
+	'version' => '1.0.37'
 );
 
 $wgMessagesDirs['Moderation'] = array(
