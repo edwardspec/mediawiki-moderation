18066ac5b2b158edb83153b65d6356affb0e758c
Edward Chernenko <edwardspec@gmail.com>
2018-04-20 16:46:55 +0300

    (refactor) Minor refactoring of wasDbUpdatedAfter()

--- util/ModerationVersionCheck.php
+++ util/ModerationVersionCheck.php
@@ -24,13 +24,13 @@ class ModerationVersionCheck {
 
 	const EXTENSION_NAME = 'Moderation'; /**< Name of extension (as listed in extension.json) */
 
-	/** @brief WHERE conditions used in wasDbUpdatedAfter(), markDbAsUpdated() */
+	/** @brief WHERE conditions used in getDbUpdatedVersionUncached(), markDbAsUpdated() */
 	protected static $where = [
 		'pp_page' => -1,
 		'pp_propname' => 'moderation:lastDbUpdateVersion'
 	];
 
-	/** @brief Returns memcached key used by wasDbUpdatedAfter() and markDbAsUpdated() */
+	/** @brief Returns memcached key used by getDbUpdatedVersion() and markDbAsUpdated() */
 	protected static function getCacheKey() {
 		return wfMemcKey( 'moderation-lastDbUpdateVersion' );
 	}
@@ -41,20 +41,27 @@ class ModerationVersionCheck {
 		@returns True if update.php was called, false otherwise.
 	*/
 	public static function wasDbUpdatedAfter( $versionOfModeration ) {
+		return version_compare( $versionOfModeration, self::getDbUpdatedVersion(), '<=' );
+	}
+
+	/**
+		@brief Returns version that Moderation had during the latest invocation of update.php.
+	*/
+	protected static function getDbUpdatedVersion() {
 		$cache = wfGetMainCache();
 		$cacheKey = self::getCacheKey();
 
 		$result = $cache->get( $cacheKey );
 		if ( $result === false ) { /* Not found in the cache */
-			$result = self::wasDbUpdatedAfterUncached();
+			$result = self::getDbUpdatedVersionUncached();
 			$cache->set( $cacheKey, $result, 86400 ); /* 24 hours */
 		}
 
-		return version_compare( $versionOfModeration, $result, '<=' );
+		return $result;
 	}
 
-	/** @brief Uncached version of wasDbUpdatedAfter(). Shouldn't be used outside of wasDbUpdatedAfter() */
-	protected static function wasDbUpdatedAfterUncached() {
+	/** @brief Uncached version of getDbUpdatedVersion(). Shouldn't be used outside of getDbUpdatedVersion() */
+	protected static function getDbUpdatedVersionUncached() {
 		$dbr = wfGetDB( DB_SLAVE );
 		return $dbr->selectField( 'page_props', 'pp_value', self::$where, __METHOD__ );
 	}
