edwardspec/mediawiki-moderation

commit ee048f0c68b393393664d0fde8fb4cefc456ec6a

(UX) Display user-friendly "Success! Uploaded!" after Special:Upload
Previously Special:Upload diplayed the message (moderation-image-queued)
as an error (red border around the message, "try again" form, etc.),
which was confusing the new users.

To avoid that, UploadVerifyUpload now throws ModerationQueuedSuccessException
to completely overwrite the post-upload page.

This doesn't happen if UploadVerifyUpload hook wasn't caused by Special:Upload,
e.g. when upload is performed by API or third-party extension.

lib/exception/ModerationQueuedSuccessException.php
@@ -1,0 +1,56 @@
+<?php
+
+/*
+	Extension:Moderation - MediaWiki extension.
+	Copyright (C) 2018 Edward Chernenko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/**
+	@file
+	@brief Displays "Upload successful! Sent for moderation!" on Special:Upload.
+
+	When we return "moderation-edit-queued" Status from upload/move hooks,
+	forms like Special:Upload and Special:MovePage often portray this as
+	some kind of fatal error. For example, MovePage says "Permission error:
+	queued for moderation", which is not user-friendly at all.
+
+	To avoid that, we throw ErrorPageError exception that completely
+	rewrites the response page, saying that Upload/Move were a success,
+	followed by explanation about moderation.
+*/
+
+class ModerationQueuedSuccessException extends ErrorPageError {
+
+	/**
+		@brief Throw this exception if the user is on a special page that needs it.
+	*/
+	public static function throwIfNeeded( $msg, array $params = [] ) {
+		$title = RequestContext::getMain()->getTitle();
+
+		/* Special:Upload treats "moderation-image-queued" as an error,
+			let's display a user-friendly results page instead */
+		if ( $title->isSpecial( 'Upload' ) ) {
+			throw new self( $msg, $params );
+		}
+	}
+
+	public function isLoggable() {
+		/* This is a successful action, not an error,
+			it doesn't belong in the error log */
+		return false;
+	}
+
+	public function __construct( $msg, $params = [] ) {
+		parent::__construct( 'moderation', $msg, $params );
+	}
+}

hooks/ModerationUploadHooks.php
@@ -62,7 +62,13 @@ class ModerationUploadHooks {
 			__METHOD__
 		);
 
-		$error = [ 'moderation-image-queued' ];
+		/* Display user-friendly results page if the upload was caused
+			by Special:Upload (not API, other extension, etc.) */
+		$errorMsg = 'moderation-image-queued';
+		ModerationQueuedSuccessException::throwIfNeeded( $errorMsg );
+
+		// Return machine-readable error if this is NOT Special:Upload.
+		$error = [ $errorMsg ];
 		return true;
 	}
 

tests/phpunit/ModerationShowTest.php
@@ -2,7 +2,7 @@
 
 /*
 	Extension:Moderation - MediaWiki extension.
-	Copyright (C) 2015-2017 Edward Chernenko.
+	Copyright (C) 2015-2018 Edward Chernenko.
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -108,7 +108,7 @@ class ModerationTestShow extends MediaWikiTestCase
 		*/
 
 		$t->loginAs( $t->unprivilegedUser );
-		$error = $t->doTestUpload( "Test image 1.png", "image640x50.png",
+		$t->doTestUpload( "Test image 1.png", "image640x50.png",
 			"" # Empty description: check for (moderation-diff-upload-notext)
 		);
 		$t->fetchSpecial();

tests/phpunit/ModerationUploadTest.php
@@ -33,11 +33,12 @@ class ModerationTestUpload extends MediaWikiTestCase
 		$t = new ModerationTestsuite();
 
 		$t->loginAs( $t->unprivilegedUser );
-		$error = $t->doTestUpload();
+		$result = $t->doTestUpload();
 		$t->fetchSpecial();
 
 		# Was the upload queued for moderation?
-		$this->assertEquals( '(moderation-image-queued)', $error );
+		$this->assertFalse( $result['error'], "testUpload(): Special:Upload displayed an error." );
+		$this->assertContains( '(moderation-image-queued)', $result['successText'] );
 
 		# Is the data on Special:Moderation correct?
 		$entry = $t->new_entries[0];
@@ -83,10 +84,10 @@ class ModerationTestUpload extends MediaWikiTestCase
 		file_put_contents( $path, '' ); # Empty
 
 		$t->loginAs( $t->unprivilegedUser );
-		$error = $t->doTestUpload( "1.png", $path );
+		$result = $t->doTestUpload( "1.png", $path );
 		unlink( $path );
 
-		$this->assertEquals( '(emptyfile)', $error );
+		$this->assertEquals( '(emptyfile)', $result['error'] );
 	}
 
 	/**
@@ -102,11 +103,11 @@ class ModerationTestUpload extends MediaWikiTestCase
 
 		# Now queue reupload for moderation
 		$t->loginAs( $t->unprivilegedUser );
-		$error = $t->doTestUpload( $title, "image100x100.png", "Text 2" );
+		$result = $t->doTestUpload( $title, "image100x100.png", "Text 2" );
 		$t->fetchSpecial();
 
 		# Was the reupload queued for moderation?
-		$this->assertEquals( '(moderation-image-queued)', $error );
+		$this->assertContains( '(moderation-image-queued)', $result['successText'] );
 
 		# Is the data on Special:Moderation correct?
 		$entry = $t->new_entries[0];
@@ -200,11 +201,11 @@ class ModerationTestUpload extends MediaWikiTestCase
 		# (situation when image description and other parameters are not in $wgRequest).
 
 		$t->loginAs( $t->unprivilegedUser );
-		$error = $t->doTestUpload();
+		$apiError = $t->doTestUpload();
 		$t->fetchSpecial();
 
 		# Was the upload queued for moderation?
-		$this->assertEquals( '(moderation-image-queued)', $error );
+		$this->assertEquals( '(moderation-image-queued)', $apiError );
 
 		# Is the data on Special:Moderation correct?
 		$entry = $t->new_entries[0];
@@ -229,10 +230,10 @@ class ModerationTestUpload extends MediaWikiTestCase
 		$t->uploadViaAPI = true;
 
 		$t->loginAs( $t->unprivilegedUser );
-		$error = $t->doTestUpload();
+		$apiError = $t->doTestUpload();
 
 		/* Uploads via API are only supported in MediaWiki 1.28+,
 			older MediaWiki should return error. */
-		$this->assertEquals( '(nouploadmodule)', $error );
+		$this->assertEquals( '(nouploadmodule)', $apiError );
 	}
 }

tests/phpunit/framework/ModerationTestsuite.php
@@ -518,7 +518,11 @@ class ModerationTestsuite
 
 	/**
 		@brief Make an upload via the usual Special:Upload, as real users do.
-		@returns Error code (e.g. '(emptyfile)') or null.
+		@returns [ 'error' => '...', 'successText' => '...' ]
+
+		In the returned array, key 'error' is an error code (e.g. '(emptyfile)')
+		or false (if not an error).
+		Key 'successText' is main HTML (if not an error) or false (if error).
 	*/
 	public function nonApiUpload( $title, $source_filename, $text )
 	{
@@ -545,11 +549,19 @@ class ModerationTestsuite
 			# so we won't remove the braces around it.
 
 			if ( $div->getAttribute( 'class' ) == 'error' ) {
-				return trim( $div->textContent ); /* Error found */
+				/* Error found */
+				return [
+					'error' => trim( $div->textContent ),
+					'successText' => false
+				];
 			}
 		}
 
-		return null; /* No errors */
+		/* No errors */
+		return [
+			'error' => false,
+			'successText' => $html->getMainText()
+		];
 	}
 
 	/**

commit 4fcb12c27e0aa0c8d80fa7253d85a950bcfb6758

(bug) Command-line scripts issue: fix error in performUpload()
This fixes issue introduced in ee048f0c68b393393664d0fde8fb4cefc456ec6a
("(UX) Display user-friendly "Success! Uploaded!" after Special:Upload").

Now ModerationQueuedSuccessException won't assume that $wgTitle is set,
because command line scripts (e.g. maintenance scripts) don't set it.

lib/exception/ModerationQueuedSuccessException.php
@@ -36,6 +36,9 @@ class ModerationQueuedSuccessException extends ErrorPageError {
 	*/
 	public static function throwIfNeeded( $msg, array $params = [] ) {
 		$title = RequestContext::getMain()->getTitle();
+		if ( !$title ) {
+			return; /* We are in the maintenance script */
+		}
 
 		/* Special:{Upload,MovePage} treat "moderation-{image,move}-queued"
 			as an error.