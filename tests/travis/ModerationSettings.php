86f2c77d7fff3a8c39b2a43fbd2492b48b0f86b9
Edward Chernenko <edwardspec@gmail.com>
2018-07-10 02:58:36 +0300

    (tests) Update ModerationSettings.php (for Travis tests)

--- tests/travis/ModerationSettings.php
+++ tests/travis/ModerationSettings.php
@@ -20,26 +20,51 @@
  * @brief Additional LocalSettings.php for Travis testing of Extension:Moderation.
  */
 
-$wgEnableUploads = true; # For upload-related tests
-wfLoadSkin( 'Vector' ); # Skin prints the notice which is tested by ModerationNotifyModeratorTest
+# URL of the testwiki (as configured in .travis.yml).
+$wgServer = "http://moderation.example.com";
+$wgScriptPath = "/mediawiki";
+$wgArticlePath = "/wiki/$1";
 
+# For upload-related tests
+$wgEnableUploads = true;
+
+# Skin prints the notice which is tested by ModerationNotifyModeratorTest
+wfLoadSkin( 'Vector' );
+
+# Object cache is needed for ModerationNotifyModeratorTest
 $wgMainCacheType = CACHE_MEMCACHED;
 $wgMemCachedServers = [ "127.0.0.1:11211" ];
 
+# Don't trigger $wgRateLimits in simultaneous Selenium tests
+$wgGroupPermissions['*']['noratelimit'] = true;
+
 # These extensions are needed for some tests of Extension:Moderation
 wfLoadExtensions( [
+	# For PHPUnit testsuite
 	'AbuseFilter',
-	'CheckUser'
+	'CheckUser',
+
+	# For Selenium testsuite
+	'MobileFrontend',
+	'VisualEditor'
 ] );
 
 if ( version_compare( $wgVersion, '1.30.0', '>=' ) ) {
 	wfLoadExtension( 'PageForms' );
 }
 
-# The following extensions are tested by Selenium testsuite,
-# they are not needed for PHPUnit testsuite to run.
-# wfLoadExtension( 'MobileFrontend' );
-# wfLoadExtension( 'VisualEditor' );
+# Default skin for Extension:MobileFrontend
+if ( version_compare( $wgVersion, '1.30.0', '>=' ) ) {
+	# MediaWiki 1.27-1.29 have this skin included into MobileFrontend
+	wfLoadSkin( 'MinervaNeue' );
+}
+
+# Parsoid configuration (used by Extension:VisualEditor)
+$wgVirtualRestConfig['modules']['parsoid'] = [
+	'url' => 'http://moderation.example.com:8142',
+	'domain' => 'moderation.example.com'
+];
+$wgDefaultUserOptions['visualeditor-enable'] = 1; # Enable VisualEditor for all users
 
 # Tested extension.
 # Note: Moderation must always be enabled LAST in LocalSettings.php.
