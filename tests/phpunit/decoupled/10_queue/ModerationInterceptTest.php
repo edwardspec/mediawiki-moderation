5899b4261f4260af7c4262cb6849d62f21521c1a
Edward Chernenko <edwardspec@gmail.com>
2018-07-15 04:02:02 +0300

    (tests) Decoupled PHPUnit testsuite: implement InterceptTest
    InterceptTest will check that moderation can be bypassed when expected,
    e.g. for automoderated users, when Moderation is disabled, when it is
    only enabled in certain namespaces, etc.
    
    It also verifies that moderation is NOT bypassed in other situations.
    
    The following configuration options are now covered by this test:
    $wgModerationIgnoredInNamespaces
    $wgModerationOnlyInNamespaces
    $wgModerationEnable

---
+++ tests/phpunit/decoupled/10_queue/ModerationInterceptTest.php
@@ -0,0 +1,270 @@
+<?php
+
+/*
+	Extension:Moderation - MediaWiki extension.
+	Copyright (C) 2018 Edward Chernenko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/**
+ * @file
+ * @brief Checks that automoderated users can bypass moderation and others can't.
+ */
+
+require_once __DIR__ . "/../../framework/ModerationTestsuite.php";
+
+/**
+ * @covers ModerationCanSkip
+ */
+class ModerationInterceptTest extends MediaWikiTestCase {
+	/**
+	 * @dataProvider dataProvider
+	 */
+	public function testIntercept( array $options ) {
+		ModerationInterceptTestSet::run( $options, $this );
+	}
+
+	/**
+	 * @brief Provide datasets for testIntercept() runs.
+	 */
+	public function dataProvider() {
+		return [
+			[ [ 'anonymously' => true ] ],
+			[ [ 'groups' => [] ] ],
+			[ [ 'intercept' => false, 'groups' => [ 'automoderated' ] ] ],
+			[ [ 'namespace' => NS_MAIN ] ],
+			[ [ 'namespace' => NS_PROJECT ] ],
+
+			// Special cases:
+			// moderator-but-not-automoderated can't skip moderation of edits,
+			// but rollback can (see ModerationCanSkip::canSkip() for explanation).
+			[ [ 'groups' => [ 'moderator' ] ] ], // can't skip
+			[ [ 'intercept' => false, 'groups' => [ 'rollback' ] ] ],
+			[ [ 'action' => 'move', 'groups' => [ 'rollback' ] ] ],
+
+			// $wgModerationEnable
+			[ [ 'intercept' => false, 'anonymously' => true, 'ModerationEnable' => false ] ],
+
+			// $wgModerationOnlyInNamespaces
+			[ [ 'namespace' => NS_MAIN,
+				'ModerationOnlyInNamespaces' => [ NS_MAIN, NS_PROJECT ] ] ],
+			[ [ 'namespace' => NS_PROJECT,
+				'ModerationOnlyInNamespaces' => [ NS_MAIN, NS_PROJECT ] ] ],
+			[ [ 'intercept' => false, 'namespace' => NS_USER,
+				'ModerationOnlyInNamespaces' => [ NS_MAIN, NS_PROJECT ] ] ],
+
+			// $wgModerationIgnoredInNamespaces
+			[ [ 'intercept' => false, 'namespace' => NS_MAIN,
+				'ModerationIgnoredInNamespaces' => [ NS_MAIN, NS_PROJECT ] ] ],
+			[ [ 'intercept' => false, 'namespace' => NS_PROJECT,
+				'ModerationIgnoredInNamespaces' => [ NS_MAIN, NS_PROJECT ] ] ],
+			[ [ 'namespace' => NS_USER,
+				'ModerationIgnoredInNamespaces' => [ NS_MAIN, NS_PROJECT ] ] ],
+
+			// Uploads
+			[ [ 'action' => 'upload' ] ],
+			[ [ 'intercept' => false, 'action' => 'upload', 'groups' => [ 'automoderated' ] ] ],
+			[ [ 'intercept' => false, 'action' => 'upload',
+				'ModerationIgnoredInNamespaces' => [ NS_FILE ] ] ],
+			[ [ 'intercept' => false, 'action' => 'upload',
+				'ModerationOnlyInNamespaces' => [ NS_MAIN ] ] ],
+
+			// Moves
+			[ [ 'action' => 'move' ] ],
+			[ [ 'intercept' => false, 'action' => 'move', 'groups' => [ 'automoderated' ] ] ],
+
+			[ [
+				// Source namespace being excluded is not enough for move to bypass moderation,
+				// both source and target must be excluded from moderation.
+				'intercept' => true,
+				'action' => 'move',
+				'ModerationIgnoredInNamespaces' => [ ModerationInterceptTestSet::DEFAULT_NS1 ]
+			] ],
+			[ [
+				// Target namespace being excluded is not enough for move to bypass moderation,
+				// both source and target must be excluded from moderation.
+				'intercept' => true,
+				'action' => 'move',
+				'ModerationIgnoredInNamespaces' => [ ModerationInterceptTestSet::DEFAULT_NS2 ]
+			] ],
+			[ [
+				'intercept' => false,
+				'action' => 'move',
+				'ModerationIgnoredInNamespaces' => [
+					ModerationInterceptTestSet::DEFAULT_NS1,
+					ModerationInterceptTestSet::DEFAULT_NS2
+				]
+			] ]
+		];
+	}
+}
+
+/**
+ * @brief Represents one TestSet for testIntercept().
+ */
+class ModerationInterceptTestSet extends ModerationTestsuiteTestSet {
+
+	/**
+	 * @const Namespace which is used when not selected by the test.
+	 */
+	const DEFAULT_NS1 = NS_USER_TALK;
+
+	/**
+	 * @const Second namespace which is used when not selected by the test.
+	 * Only needed for moves.
+	 */
+	const DEFAULT_NS2 = NS_HELP;
+
+	/** @var bool If true, this change is expected to be intercepted. */
+	protected $intercept = true;
+
+	/** @var int Namespace of the test page */
+	protected $namespace = self::DEFAULT_NS1;
+
+	/** @var int Namespace of the new title (for moves) */
+	protected $namespace2 = self::DEFAULT_NS2;
+
+	/** @var array|null Extra configuration, e.g. [ 'ModerationIgnoredInNamespaces' => [] ] */
+	protected $configVars = [];
+
+	/** @var array Groups of the test user, e.g. [ 'sysop', 'bureaucrat' ] */
+	protected $groups = [];
+
+	/** @var bool If true, the edit will be anonymous. ($groups will be ignored) */
+	protected $anonymously = false;
+
+	/** @var string Operation to test, one of the following: 'edit', 'upload', 'move' */
+	protected $action = 'edit';
+
+	/** @var string|array|null Result of apiEdit(), apiMove(), apiUpload(), if any */
+	protected $result = null;
+
+	/**
+	 * @brief Initialize this TestSet from the input of dataProvider.
+	 */
+	protected function applyOptions( array $options ) {
+		foreach ( $options as $key => $value ) {
+			switch ( $key ) {
+
+				case 'ModerationEnable':
+				case 'ModerationIgnoredInNamespaces':
+				case 'ModerationOnlyInNamespaces':
+					$this->configVars[$key] = $value;
+					break;
+
+				case 'intercept':
+				case 'namespace':
+				case 'namespace2':
+				case 'action':
+				case 'groups':
+				case 'anonymously':
+					$this->$key = $value;
+					break;
+
+				default:
+					throw new Exception( __CLASS__ . ": unknown key {$key} in options" );
+			}
+		}
+
+		/* Default options */
+		if ( $this->action == 'upload' &&
+			ModerationTestsuite::mwVersionCompare( '1.28.0', '<' )
+		) {
+			$this->getTestcase()->markTestSkipped(
+				'Test skipped: MediaWiki 1.27 doesn\'t support upload via API.' );
+		}
+	}
+
+	/**
+	 * @brief Assert the state of the database after the edit.
+	 */
+	protected function assertResults( MediaWikiTestCase $testcase ) {
+		$dbw = wfGetDB( DB_MASTER );
+		$row = $dbw->selectRow( 'moderation', '*', '', __METHOD__ );
+
+		$this->getTestcase()->assertEquals(
+			[ 'edit was intercepted' => $this->intercept ],
+			[ 'edit was intercepted' => (bool)$row ]
+		);
+
+		/* TODO: check $this->result */
+
+		/* TODO: write a uniform class for return values of $t->apiMove(), $t->nonApiMove(), etc.,
+			so that we could just write $result->wasIntercepted()
+			and not reinvent the complex checks in the tests themselves.
+		*/
+	}
+
+	/**
+	 * @brief Execute the TestSet, making an edit/upload/move with requested parameters.
+	 */
+	protected function makeChanges() {
+		$testcase = $this->getTestcase();
+		$t = $this->getTestsuite();
+
+		foreach ( $this->configVars as $name => $value ) {
+			$t->setMwConfig( $name, $value );
+		}
+
+		$title = Title::makeTitle( $this->namespace, 'Test page 1' );
+		$page2Title = Title::makeTitle( $this->namespace2, 'Test page 2' );
+
+		$user = User::newFromName( '127.0.0.1', false );
+		if ( !$this->anonymously ) {
+			$user = $t->unprivilegedUser;
+
+			// Make sure that the test user doesn't have any groups
+			foreach ( $user->getGroups() as $oldGroup ) {
+				$user->removeGroup( $oldGroup );
+			}
+
+			// Add the user to groups requested by this test (if any)
+			foreach ( $this->groups as $group ) {
+				$user->addGroup( $group );
+
+				if ( $group == 'rollback' ) {
+					// Rollback group (users with 'rollback' right) is not defined
+					// by default, so we need to configure it explicitly.
+					global $wgGroupPermissions;
+					$t->setMwConfig( 'GroupPermissions',
+						[ 'rollback' => [ 'rollback' => true ] ] + $wgGroupPermissions );
+				}
+			}
+		}
+
+		if ( $this->action == 'move' ) {
+			ModerationTestUtil::fastEdit( $title );
+		}
+
+		$t->loginAs( $user );
+
+		switch ( $this->action ) {
+			case 'edit':
+				$this->result = $t->apiEdit( $title->getFullText(), 'New text', 'Summary' );
+				break;
+
+			case 'move':
+				$this->result = $t->apiMove(
+					$title->getFullText(),
+					$page2Title->getFullText()
+				);
+				break;
+
+			case 'upload':
+				$this->result = $t->apiUpload( $title->getText(), '', '' );
+				break;
+
+			default:
+				throw MWException( 'Unknown "action" requested in TestSet of ' . __CLASS__ );
+		}
+	}
+}
