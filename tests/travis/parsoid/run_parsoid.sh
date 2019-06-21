59a90ca37f970ff60f9d3476f9d87b99e8e1ce85
Edward Chernenko <edwardspec@gmail.com>
2019-06-21 03:13:17 +0300

    (tests) Travis: use Parsoid 0.9 when testing with MediaWiki 1.31
    Newer Parsoid 0.10 doesn't support VisualEditor for MediaWiki 1.31.

--- tests/travis/parsoid/run_parsoid.sh
+++ tests/travis/parsoid/run_parsoid.sh
@@ -8,20 +8,19 @@
 branch=$1
 GITCLONE_OPTS="--recurse-submodules -j 5"
 
-if [ "$branch" != "REL1_27" ]; then
-	# See below, REL1_27 can't use --depth 1,
-	# because it would need to revert Parsoid to older revision.
-	GITCLONE_OPTS="--depth 1 $GITCLONE_OPTS"
-fi
-
 if [ ! -f parsoid/COMPLETE ]; then
 	rm -rf parsoid
 	git clone $GITCLONE_OPTS https://gerrit.wikimedia.org/r/p/mediawiki/services/parsoid/deploy parsoid
 
+	# Older MediaWiki 1.27 doesn't support latest Parsoid.
+	# Revert Parsoid to the supported version.
 	if [ "$branch" = "REL1_27" ]; then
-		# Legacy MediaWiki 1.27 doesn't support latest Parsoid.
-		# Revert Parsoid to the supported version.
 		PARSOID_DEPLOY_REVISION=205ae95d46d2452c2c7c2302e77a59e6ddef3afb
+	else if [ "$branch" = "REL1_31" ]; then
+		PARSOID_DEPLOY_REVISION=1cc68445c46759d0149bc831f0330d2229885d87
+	fi; fi
+
+	if [ "x$PARSOID_DEPLOY_REVISION" != "x" ]; then
 		( cd parsoid && git checkout --recurse-submodules $PARSOID_DEPLOY_REVISION )
 	fi
 
