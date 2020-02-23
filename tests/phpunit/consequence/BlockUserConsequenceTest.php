e1f4c31b095e898af0a01ac9b983e15cd4326b8b
Edward Chernenko <edwardspec@gmail.com>
2020-02-23 03:08:02 +0300

    (tests) Implement unit tests of {Block,Unblock}UserConsequence

---
+++ tests/phpunit/consequence/BlockUserConsequenceTest.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+	Extension:Moderation - MediaWiki extension.
+	Copyright (C) 2020 Edward Chernenko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/**
+ * @file
+ * Unit test of BlockUserConsequence.
+ */
+
+use MediaWiki\Moderation\BlockUserConsequence;
+
+/**
+ * @group Database
+ */
+class BlockUserConsequenceTest extends MediaWikiTestCase {
+
+	/** @var string[] */
+	protected $tablesUsed = [ 'moderation_block', 'user' ];
+
+	/**
+	 * Verify that BlockUserConsequence adds a block to the database.
+	 * @param int $userId
+	 * @param string $username
+	 * @param string $moderatorName
+	 * @covers MediaWiki\Moderation\BlockUserConsequence
+	 * @dataProvider dataProviderBlockUser
+	 */
+	public function testBlockUser( $userId, $username, $moderatorName ) {
+		$moderator = User::createNew( $moderatorName );
+
+		// Create and run the Consequence.
+		$consequence = new BlockUserConsequence( $userId, $username, $moderator );
+		$somethingChanged = $consequence->run();
+
+		$this->assertTrue( $somethingChanged );
+
+		// New row should have appeared in the database.
+		$this->assertBlockRecorded( $userId, $username, $moderator );
+	}
+
+	/**
+	 * Verify that BlockUserConsequence on already blocked user returns false.
+	 * @param int $userId
+	 * @param string $username
+	 * @param string $moderatorName
+	 * @covers MediaWiki\Moderation\BlockUserConsequence
+	 * @dataProvider dataProviderBlockUser
+	 */
+	public function testNoopBlockUser( $userId, $username, $moderatorName ) {
+		$moderator = User::createNew( $moderatorName );
+
+		// Create and run the Consequence.
+		$consequence1 = new BlockUserConsequence( $userId, $username, $moderator );
+		$consequence1->run();
+
+		$consequence2 = new BlockUserConsequence( $userId, $username, $moderator );
+		$somethingChanged = $consequence2->run();
+		$this->assertFalse( $somethingChanged );
+
+		// Despite $consequence2 doing nothing, the row should still exist in the database.
+		$this->assertBlockRecorded( $userId, $username, $moderator );
+	}
+
+	/**
+	 * Assert that the block exists in the database.
+	 * @param int $userId
+	 * @param string $username
+	 * @param User $moderator
+	 */
+	private function assertBlockRecorded( $userId, $username, User $moderator ) {
+		$this->assertSelect( 'moderation_block',
+			[
+				'mb_user',
+				'mb_by',
+				'mb_by_text'
+			],
+			[ 'mb_address' => $username ],
+			[ [
+				$userId,
+				$moderator->getId(),
+				$moderator->getName()
+			] ]
+		);
+	}
+
+	/**
+	 * Provide datasets for testBlockUser() and testNoopBlockUser() runs.
+	 */
+	public function dataProviderBlockUser() {
+		return [
+			'anonymous user' => [ 0, '10.11.12.13', 'First moderator' ],
+			'registered user' => [ 1234, 'Registered user (ID 1234)', 'Second moderator' ],
+		];
+	}
+}
---
+++ tests/phpunit/consequence/UnblockUserConsequenceTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+	Extension:Moderation - MediaWiki extension.
+	Copyright (C) 2020 Edward Chernenko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/**
+ * @file
+ * Unit test of UnblockUserConsequence.
+ */
+
+use MediaWiki\Moderation\BlockUserConsequence;
+use MediaWiki\Moderation\UnblockUserConsequence;
+
+/**
+ * @group Database
+ */
+class UnblockUserConsequenceTest extends MediaWikiTestCase {
+
+	/** @var string[] */
+	protected $tablesUsed = [ 'moderation_block', 'user' ];
+
+	/**
+	 * Verify that UnblockUserConsequence returns false if the user wasn't blocked to begin with.
+	 * @param string $username
+	 * @covers MediaWiki\Moderation\BlockUserConsequence
+	 * @dataProvider dataProviderUnblockUser
+	 */
+	public function testNoopUnblockUser( $username ) {
+		// Create and run the Consequence.
+		$consequence = new UnblockUserConsequence( $username );
+		$somethingChanged = $consequence->run();
+
+		$this->assertFalse( $somethingChanged );
+		$this->assertNotBlocked( $username );
+	}
+
+	/**
+	 * Verify that UnblockUserConsequence removes an existing block from the database.
+	 * @param string $username
+	 * @covers MediaWiki\Moderation\UnblockUserConsequence
+	 * @dataProvider dataProviderUnblockUser
+	 */
+	public function testUnblockUser( $username ) {
+		// Make a currently blocked user.
+		$user = User::createNew( $username );
+		$moderator = User::createNew( 'Some moderator' );
+		$blockConsequence = new BlockUserConsequence( $user->getId(), $username, $moderator );
+		$blockConsequence->run();
+
+		// Create and run the Consequence.
+		$consequence = new UnblockUserConsequence( $username );
+		$somethingChanged = $consequence->run();
+
+		$this->assertTrue( $somethingChanged );
+		$this->assertNotBlocked( $username );
+	}
+
+	/**
+	 * Assert that the block doesn't exist in the database.
+	 */
+	private function assertNotBlocked( $username ) {
+		$this->assertSelect( 'moderation_block',
+			[ 'mb_id' ],
+			[ 'mb_address' => $username ],
+			[] // Expected result: no rows selected.
+		);
+	}
+
+	/**
+	 * Provide datasets for testUnblockUser() and testNoopUnblockUser() runs.
+	 */
+	public function dataProviderUnblockUser() {
+		return [
+			'anonymous user' => [ '10.11.12.13' ],
+			'registered user' => [ 'Registered user (ID 1234)' ]
+		];
+	}
+}
