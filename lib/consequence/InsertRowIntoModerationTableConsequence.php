562bcc064ded6fdbf556442eed527d606d889e85
Edward Chernenko <edwardspec@gmail.com>
2020-03-04 13:01:43 +0300

    (lib) Fix return value of InsertRowIntoModerationTable in PostgreSQL
    It was noticed that $dbw->insertId() is incorrect (doesn't correspond
    to mod_id of updated row) due to non-native $dbw->upsert() being used
    for PostgreSQL by MediaWiki.
    (it returns insertId() from noop INSERT IGNORE instead of insertId()
    from UPDATE, which is what we need, but can't obtain)
    
    Note: this return value isn't really used anywhere except tests
    (not since ModerationNewChange::upload() method was implemented),
    so it's a shame that it requires an extra SELECT query.
    
    However, returning incorrect row number would be undocumented behavior,
    and we are still fixing this for consistency.
    
    Note: MySQL is unaffected by this change. (it uses native UPSERT)
    
    Note: for PostgreSQL, performance impact is negligible,
    as this is an indexed SELECT query for a newly inserted row,
    and it only happens during a slow non-readonly action (editing a page).

--- lib/consequence/InsertRowIntoModerationTableConsequence.php
+++ lib/consequence/InsertRowIntoModerationTableConsequence.php
@@ -60,12 +60,26 @@ class InsertRowIntoModerationTableConsequence implements IConsequence {
 			__METHOD__
 		] );
 
-		// FIXME: we can't rely on $dbw->insertId() for PostgreSQL, because $dbw->upsert()
-		// doesn't use native UPSERT, and instead does UPDATE and then INSERT IGNORE.
-		// Since values of PostgreSQL sequences always increase (even after no-op INSERT IGNORE),
-		// $dbw->insertId() will return the sequence number after INSERT.
-		// But if changes were caused by UPDATE (not by INSERT), then this number won't be correct
-		// (we would want insertId() from UPDATE, which is lost during $dbw->upsert()).
+		if ( $dbw->getType() == 'postgres' ) {
+			// It's a bit of a shame to do an extra SELECT query just for that,
+			// but we can't rely on $dbw->insertId() for PostgreSQL, because $dbw->upsert()
+			// doesn't use native UPSERT, and instead does UPDATE and then INSERT IGNORE.
+			// Since values of PostgreSQL sequences always increase (even after no-op
+			// INSERT IGNORE), insertId() will return the sequence number after INSERT.
+			// But if changes were caused by UPDATE (not by INSERT), then this number
+			// won't be correct (we would want insertId() from UPDATE,
+			// which is lost during $dbw->upsert()).
+			//
+			// A better solution would probably be to require PosgreSQL 9.5 or later and
+			// use the native UPSERT (MediaWiki core doesn't do so, as it keeps backward
+			// compatibility with PostgreSQL 9.2).
+			$where = [];
+			foreach ( $uniqueFields as $field ) {
+				$where[$field] = $this->fields[$field];
+			}
+
+			return (int)$dbw->selectField( 'moderation', 'mod_id', $where, __METHOD__ );
+		}
 
 		return $dbw->insertId();
 	}
