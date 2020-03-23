59a1b22d2e166c086fdb5bf47feb49e7f594d5e2
Edward Chernenko <edwardspec@gmail.com>
2020-03-24 02:09:02 +0300

    (tests) Implement unit test of ModerationBlockCheck class

---
+++ tests/phpunit/consequence/ModerationBlockCheckTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+	Extension:Moderation - MediaWiki extension.
+	Copyright (C) 2020 Edward Chernenko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/**
+ * @file
+ * Unit test of ModerationBlockCheck.
+ */
+
+require_once __DIR__ . "/autoload.php";
+
+/**
+ * @group Database
+ */
+class ModerationBlockCheckTest extends ModerationUnitTestCase {
+	protected $tablesUsed = [ 'moderation_block' ];
+
+	/**
+	 * Test that ModerationBlockCheck::isModerationBlocked() returns correct value.
+	 * @covers ModerationBlockCheck
+	 */
+	public function testIsBlocked() {
+		$blockedUser = User::newFromName( 'Some blocked user ' . rand( 0, 100000 ), false );
+		$notBlockedUser = User::newFromName( 'Not blocked ' . rand( 0, 100000 ), false );
+
+		$this->db->insert( 'moderation_block', [
+			'mb_address' => $blockedUser->getName(),
+			'mb_user' => 0,
+			'mb_by' => 123,
+			'mb_by_text' => 'Some moderator',
+			'mb_timestamp' => $this->db->timestamp()
+		], __METHOD__ );
+
+		$this->assertTrue( ModerationBlockCheck::isModerationBlocked( $blockedUser ) );
+		$this->assertFalse( ModerationBlockCheck::isModerationBlocked( $notBlockedUser ) );
+
+		// isModerationBlocked() should return false if the user has been unblocked.
+		$this->db->delete( 'moderation_block',
+			[ 'mb_address' => $blockedUser->getName() ],
+			__METHOD__
+		);
+		$this->assertFalse( ModerationBlockCheck::isModerationBlocked( $blockedUser ) );
+	}
+}
