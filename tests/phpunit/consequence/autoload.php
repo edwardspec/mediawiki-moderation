df105e988a1eb2b29cf3f66c8bca369f070c6a33
Edward Chernenko <edwardspec@gmail.com>
2020-03-29 12:07:49 +0300

    mv tests/phpunit/consequence/{*Trait.php,trait/}

--- tests/phpunit/consequence/autoload.php
+++ tests/phpunit/consequence/autoload.php
@@ -25,11 +25,11 @@ global $wgAutoloadClasses;
 $wgAutoloadClasses += [
 	'ModerationTestHTML' => __DIR__ . '/../../common/ModerationTestHTML.php',
 	'ModerationTestUtil' => __DIR__ . '/../../common/ModerationTestUtil.php',
-	'ConsequenceTestTrait' => __DIR__ . '/ConsequenceTestTrait.php',
-	'MakeEditTestTrait' => __DIR__ . '/MakeEditTestTrait.php',
-	'MockRevisionLookupTestTrait' => __DIR__ . '/MockRevisionLookupTestTrait.php',
+	'ConsequenceTestTrait' => __DIR__ . '/trait/ConsequenceTestTrait.php',
+	'MakeEditTestTrait' => __DIR__ . '/trait/MakeEditTestTrait.php',
+	'MockRevisionLookupTestTrait' => __DIR__ . '/trait/MockRevisionLookupTestTrait.php',
 	'MediaWiki\\Moderation\\MockConsequenceManager' => __DIR__ . '/MockConsequenceManager.php',
 	'ModerationUnitTestCase' => __DIR__ . '/ModerationUnitTestCase.php',
-	'ModifyDbRowTestTrait' => __DIR__ . '/ModifyDbRowTestTrait.php',
-	'UploadTestTrait' => __DIR__ . '/UploadTestTrait.php',
+	'ModifyDbRowTestTrait' => __DIR__ . '/trait/ModifyDbRowTestTrait.php',
+	'UploadTestTrait' => __DIR__ . '/trait/UploadTestTrait.php',
 ];
