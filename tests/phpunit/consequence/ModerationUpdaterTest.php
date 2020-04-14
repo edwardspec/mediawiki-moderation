d0bd2a88ee6e26883ef9662f59152f4729dbd7e2
Edward Chernenko <edwardspec@gmail.com>
2020-04-14 11:56:17 +0300

    (tests) Implement unit test of ModerationUpdater

---
+++ tests/phpunit/consequence/ModerationUpdaterTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+	Extension:Moderation - MediaWiki extension.
+	Copyright (C) 2020 Edward Chernenko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/**
+ * @file
+ * Unit test of ModerationUpdater.
+ */
+
+use Wikimedia\Rdbms\IDatabase;
+
+require_once __DIR__ . "/autoload.php";
+
+class ModerationUpdaterTest extends ModerationUnitTestCase {
+
+	/**
+	 * Verify that our LoadExtensionSchemaUpdates hook adds the necessary database updates.
+	 * @covers ModerationUpdater
+	 */
+	public function testHook() {
+		$updater = $this->createMock( DatabaseUpdater::class );
+		$db = $this->createMock( IDatabase::class );
+
+		$updater->expects( $this->any() )->method( 'getDB' )->willReturn( $db );
+		$db->expects( $this->any() )->method( 'getType' )->willReturn( 'postgres' );
+
+		$updater->expects( $this->exactly( 2 ) )->method( 'addExtensionTable' )->withConsecutive(
+			[ $this->identicalTo( 'moderation' ), $this->fileExists() ],
+			[ $this->identicalTo( 'moderation_block' ), $this->fileExists() ]
+		);
+		$updater->expects( $this->any() )->method( 'addExtensionUpdate' )->with(
+			$this->identicalTo( [ 'ModerationVersionCheck::invalidateCache' ] )
+		);
+
+		// TODO: check both mysql and postgresql.
+		// TODO: check invocations of addExtensionField(), modifyExtensionField(), etc.
+
+		'@phan-var DatabaseUpdater $updater';
+
+		$hookResult = ModerationUpdater::onLoadExtensionSchemaUpdates( $updater );
+		$this->assertTrue( $hookResult,
+			'Handler of LoadExtensionSchemaUpdates hook should return true.' );
+	}
+
+	/**
+	 * Ensure that the Updater hook is installed.
+	 * We make this into a separate test, so that testHook() can just check our own handler
+	 * without invoking LoadExtensionSchemaUpdates handlers of all other extensions.
+	 * @coversNothing
+	 */
+	public function testHookInstalled() {
+		$this->assertContains(
+			'ModerationUpdater::onLoadExtensionSchemaUpdates',
+			Hooks::getHandlers( 'LoadExtensionSchemaUpdates' ),
+			"Hook handler is not installed."
+		);
+	}
+}
