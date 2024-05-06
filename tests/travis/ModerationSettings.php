3ea96aa7777cbcee1d697fab9210f5a3e51bd699
Edward Chernenko <edwardspec@gmail.com>
2024-05-06 16:58:10 +0300

    (tests) Temporarily disable AbuseFilter tests

--- tests/travis/ModerationSettings.php
+++ tests/travis/ModerationSettings.php
@@ -63,7 +63,7 @@ if ( $wgDBtype != 'postgres' ) {
 	// FIXME: what AbuseFilter does in its "UnitTestsAfterDatabaseSetup" and
 	// "UnitTestsBeforeDatabaseTeardown" hooks interferes with CliEngine.
 	// Until a workaround is found, we can't test with it.
-	wfLoadExtension( 'AbuseFilter' ); # For PHPUnit testsuite
+	// wfLoadExtension( 'AbuseFilter' ); # For PHPUnit testsuite
 }
 
 // wfLoadExtension( 'CheckUser' ); # For PHPUnit testsuite
